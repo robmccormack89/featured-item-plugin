dc856c1d58fcfa0d06fddb6aa51090d8fddc0947
robmccormack89 <robertmccormack147@gmail.com>
2021-06-24 19:15:48 +0100

    truly

--- featured-item-plugin.php
+++ featured-item-plugin.php
@@ -17,15 +17,15 @@ Domain Path: /languages/
 defined('ABSPATH') || exit;
 
 // define some constants
-if (!defined('VIDEO_POPUP_PATH')) define('VIDEO_POPUP_PATH', plugin_dir_path( __FILE__ ));
-if (!defined('VIDEO_POPUP_URL')) define('VIDEO_POPUP_URL', plugin_dir_url( __FILE__ ));
-if (!defined('VIDEO_POPUP_BASE')) define('VIDEO_POPUP_BASE', dirname(plugin_basename( __FILE__ )));
+if (!defined('FEATURED_ITEM_PATH')) define('FEATURED_ITEM_PATH', plugin_dir_path( __FILE__ ));
+if (!defined('FEATURED_ITEM_URL')) define('FEATURED_ITEM_URL', plugin_dir_url( __FILE__ ));
+if (!defined('FEATURED_ITEM_BASE')) define('FEATURED_ITEM_BASE', dirname(plugin_basename( __FILE__ )));
 
 // require the composer autoloader
 if (file_exists($composer_autoload = __DIR__.'/vendor/autoload.php')) require_once $composer_autoload;
 
 // then require the main plugin class. this class extends Timber/Timber which is required via composer
-new Rmcc\VideoPopup;
+new Rmcc\FeaturedItem;
 
 // require action functions 
 require_once('inc/functions.php');
